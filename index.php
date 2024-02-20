d26085b98a3546c3a5cf4e8d1ac4b28b86281c71
Samia Bougtoub <bougtoub.samia.solicode@gmail.com>
2024-02-20 12:07:20 +0100

    creer le dossier farhaEvents

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evenements</title>
+</head>
+<body>
+    <h1>Upcoming events</h1>
+</body>
+</html>
